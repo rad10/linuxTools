ef6ead2de630b83d81140efc04e9b10c5147e4dc
Rad10 <nrc1mr.e3@gmail.com>
2018-03-13 22:34:08 -0400

    Removed transport vial file

--- arpmap/arp.py
+++ arpmap/arp.py
@@ -1,5 +1,7 @@
-#!/bin/bash
-iplist=open("arplist.txt","r").read()
+#!/bin/python
+from sys import argv, exit
+iplist=str(argv[1])
+#iplist=open("arplist.txt","r").read()
 ip=iplist.split("\n")
 for i in range(0,len(ip)):
 	ip[i]=ip[i].split("\t")
