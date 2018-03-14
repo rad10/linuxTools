58bd4942b9154adb70dd08eff51856178ecb3187
Rad10 <nrc1mr.e3@gmail.com>
2018-03-13 22:31:48 -0400

    added sudo to required install commands

--- arpmap/install.sh
+++ arpmap/install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-if [ "$(id -u)" != "0" ]; then
-	echo "Sorry, you must be in sudo to run this."
-	exit 1
-fi
-cp arpmap /usr/local/bin/
+#if [ "$(id -u)" != "0" ]; then
+#	echo "Sorry, you must be in sudo to run this."
+#	exit 1
+#fi
+sudo cp arpmap /usr/local/bin/
 mkdir ~/.arpip/
 cp arp.py ~/.arpip/
