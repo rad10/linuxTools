df0269b3d9c7ec57176faea8b61e4319f35072a0
Rad10 <nrc1mr.e3@gmail.com>
2019-06-12 11:41:23 -0400

    An attempt to format as many files as possible

--- ipisalive.py
+++ ipisalive.py
@@ -9,12 +9,17 @@ ip = []
 opts = ["-F"]
 #[/InitConfig]#
 #[Help]#
+
+
 def help():
     print("isipalive.py [HOSTS]")
     print("isipalive.py is a function script. input an ip address and it will return 1 or 0 for true or false is the address is alive.")
     exit()
+
+
 #[/Help]#
 #[Config]#
 for i in argv[1:]:
-    if (i=="-d" or i=="-display" or i=="--display"): d=True
+    if (i == "-d" or i == "-display" or i == "--display"):
+        d = True
 #[/Config]#
